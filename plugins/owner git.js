257a55c761b87d7472edc955e257dab7585cb6e2
Anees-lub <87905109+Anees-lub@users.noreply.github.com>
2021-07-27 12:22:27 +0530

    Update owner git.js

--- plugins/owner git.js
+++ plugins/owner git.js
@@ -1,63 +1,149 @@
 const asena = require('../events');
+
 const {MessageType} = require('@adiwajshing/baileys');
+
 const OWNER = "it sends details of owner"
+
 const GIT = "it sends links"
+
 const Config = require('../config');
 
-
 if (Config.WORKTYPE == 'private') {
+
         asena.addCommand({pattern: 'owner', fromMe: true, deleteCommand: true, desc: OWNER,}, (async (message, match) => {
 
     var r_text = new Array ();
-    
-    r_text[1] = "*╔═════😎Pikachu Modified😎═════╗*\n           \n*⚜═Anees-Lub═⚜*\n\n*owner AneesAns- http://Wa.me/917356378949*\n*          *\n*╚══════🔱🔱🔱🔱🔱═════╝*\n\n*▷Creator: Anees-Lub*"
 
     
+
+    r_text[1] = "*╔═════😎Pikachu😎═════╗*\n           \n*⚜═Pikachu═⚜*\n\n*owner AmeerSuhail - http://Wa.me/+919946432377*\n* *\n🔰instagram:-https://www.instagram.com/ameer_.su_hail?r=nametag*            *\n*╚══════🔱🔱🔱🔱🔱═════╝*\n\n*▷Creator: AmeerSuhail*"
+
+    
+
     await message.client.sendMessage(
+
         message.jid,(r_text[1]), MessageType.text);
 
     }));
-
 
         asena.addCommand({pattern: 'git', fromMe: true, deleteCommand: true, desc: GIT,}, (async (message, match) => {
 
         var r_text = new Array ();
-    
-        r_text[1] = "*Git links*\n           *\n💥═Pikachu Owner Anees-Lub═💥*\n\n*💘https://github.com/ameer-kallumthodi/pikachu*\n*"
 
     
-        await message.client.sendMessage(
-            message.jid,(r_text[1]), MessageType.text);
+
+        r_text[1] = "*Git links*\n           *\n💥═Pikachu Owner Ameer Suhail═💥*\n\n*💘https://github.com/ameer-kallumthodi/pikachu*\n*     *\n💓Liyamol═Owner Saidali═*\n\n*⚜https://github.com/saidalisaid2/LiyaMol*    *\n\n⚜yt link to set bot:- https://youtu.be/_D4ZYuUSXjs*"
+
     
+
+        await message.client.sendMessage(
+
+            message.jid,(r_text[1]), MessageType.text);
+
+    
+
         }));    
 
     }
+
     
 
     if (Config.WORKTYPE == 'public') {
+
         asena.addCommand({pattern: 'owner', fromMe: false, deleteCommand: true, desc: OWNER,}, (async (message, match) => {
 
     var r_text = new Array ();
-    
-    r_text[1] = "*╔═════😎Pikachu Modified😎═════╗*\n           \n*⚜═Pikachu═⚜*\n\n*owner Anees-Lub - http://Wa.me/917356378949*\n*            *\n*╚══════🔱🔱🔱🔱🔱═════╝*\n\n*▷Creator: Anees-Lub*"
 
     
+
+    r_text[1] = "*╔═════😎Pikachu😎═════╗*\n           \n*⚜═Pikachu═⚜*\n\n*owner AmeerSuhail - http://Wa.me/+919946432377*\n* *\n🔰instagram:-https://www.instagram.com/ameer_.su_hail?r=nametag*            *\n*╚══════🔱🔱🔱🔱🔱═════╝*\n\n*▷Creator: AmeerSuhail*"
+
+    
+
     await message.client.sendMessage(
+
         message.jid,(r_text[1]), MessageType.text);
 
     }));
 
-
         asena.addCommand({pattern: 'git', fromMe: false, deleteCommand: true, desc: GIT,}, (async (message, match) => {
 
         var r_text = new Array ();
-    
-        r_text[1] = "*Git links*\n           *\n💥═Pikachu Owner AnessLub═💥*\n\n*💘https://github.com/ameer-kallumthodi/pikachu*\n*     
 
     
-        await message.client.sendMessage(
-            message.jid,(r_text[1]), MessageType.text);
+
+        r_text[1] = "*Git links*\n           *\n💥═Pikachu Owner Ameer Suhail═💥*\n\n*💘https://github.com/ameer-kallumthodi/pikachu*\n*     *\n💓Liyamol═Owner Saidali═*\n\n*⚜https://github.com/saidalisaid2/LiyaMol*    *\n\n⚜yt link to set bot:- https://youtu.be/_D4ZYuUSXjs*"
+
     
+
+        await message.client.sendMessage(
+
+            message.jid,(r_text[1]), MessageType.text);
+
+    
+
         }));    
 
     }
+
+
+
+
+
+
+
+
+
+
+    
+
+
+    
+
+
+
+
+
+
+
+
+
+    
+
+
+    
+
+
+    
+
+
+
+
+
+
+
+
+
+    
+
+
+    
+
+
+
+ 
+
+
+
+
+
+    
+         
+
+    
+
+
+ 
+   
+
+
