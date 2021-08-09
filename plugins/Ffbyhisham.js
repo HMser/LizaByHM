66bf4867c7a18fcd46d4081ddff6f2dba4c52e3a
Hisham-Muhammed <87166264+Hisham-Muhammed@users.noreply.github.com>
2021-08-09 08:49:25 +0530

    Update and rename zholo.js to Ffbyhisham.js

--- plugins/Ffbyhisham.js
+++ plugins/Ffbyhisham.js
@@ -10,11 +10,11 @@ const need = "Command Kazhinn Yanthankilum yazhuth muthe\n🗡🗡"
 
 if (Config.WORKTYPE == 'private') {
 
-  Asena.addCommand({ pattern: 'zholo ?(.*)', fromMe: true, dontAddCommandList: true }, (async (message, match) => {
+  Asena.addCommand({ pattern: 'ff ?(.*)', fromMe: true, dontAddCommandList: true }, (async (message, match) => {
 
     if (match[1] === '') return await message.sendMessage(need);
 
-    var ttinullimage = await axios.get(`https://api.lolhuman.xyz/api/textprome/holographic?apikey=qamdi5652&text=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
+    var ttinullimage = await axios.get(`https://api.lolhuman.xyz/api/ephoto1/freefire?apikey=queenamdipublic&text=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'Made By Sophia' })
 
@@ -23,11 +23,11 @@ if (Config.WORKTYPE == 'private') {
 
 else if (Config.WORKTYPE == 'public') {
 
-  Asena.addCommand({ pattern: 'zholo ?(.*)', fromMe: false, dontAddCommandList: true }, (async (message, match) => {
+  Asena.addCommand({ pattern: 'ff ?(.*)', fromMe: false, dontAddCommandList: true }, (async (message, match) => {
 
     if (match[1] === '') return await message.sendMessage(need);
 
-    var ttinullimage = await axios.get(`https://api.lolhuman.xyz/api/textprome/holographic?apikey=qamdi5652&text=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
+    var ttinullimage = await axios.get(`https://api.lolhuman.xyz/api/ephoto1/freefire?apikey=queenamdipublic&text=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'Made By Sophia' })
 
