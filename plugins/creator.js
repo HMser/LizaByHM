51da05d0174ab9c59c169f440f4fa5ca940b9c6c
Hi it's me Anees lub <87905109+Anees-lub@users.noreply.github.com>
2021-08-10 10:25:45 +0530

    Create creator.js

---
+++ plugins/creator.js
@@ -0,0 +1,87 @@
+const asena = require('../events');
+
+const {MessageType} = require('@adiwajshing/baileys');
+
+const OWNER = "it sends details of owner"
+
+const GIT = "it sends links"
+
+const Config = require('../config');
+
+if (Config.WORKTYPE == 'private') {
+
+        asena.addCommand({pattern: 'owner', fromMe: true, deleteCommand: true, desc: OWNER,}, (async (message, match) => {
+
+    var r_text = new Array ();
+
+    
+
+    r_text[1] = "*╔═════😎Anees-Lub😎═════╗*\n           \n*⚜═AneesLub═⚜*\n\n*owner Anees - http://Wa.me/+917356378949*\n* *\n🔰instagram:- : synazX.op*            *\n*╚══════🔱🔱🔱🔱🔱═════╝*\n\n*▷Creator: Anees-Muhammed*"
+
+    
+
+    await message.client.sendMessage(
+
+        message.jid,(r_text[1]), MessageType.text);
+
+    }));
+
+        asena.addCommand({pattern: 'git', fromMe: true, deleteCommand: true, desc: GIT,}, (async (message, match) => {
+
+        var r_text = new Array ();
+
+    
+
+        r_text[1] = "*Git links*\n           *\n💥═AneesLub Owner Anees-Muhammed═💥*\n\n*💘 https://github.com/Anees-lub/Pikachu*\n*     *\n💓Liyamol═Owner Saidali═*\n\n*⚜https://github.com/saidalisaid2/LiyaMol*    *\n\n⚜yt link to set bot:- https://youtu.be/_D4ZYuUSXjs*"
+
+    
+
+        await message.client.sendMessage(
+
+            message.jid,(r_text[1]), MessageType.text);
+
+    
+
+        }));    
+
+    }
+
+    
+
+    if (Config.WORKTYPE == 'public') {
+
+        asena.addCommand({pattern: 'creator', fromMe: false, deleteCommand: true, desc: OWNER,}, (async (message, match) => {
+
+    var r_text = new Array ();
+
+    
+
+    r_text[1] = "*╔═════😎AneesLub😎═════╗*\n           \n*⚜═Sophia═⚜*\n\n*owner Anees - http://Wa.me/+917356378949*\n* *\n🔰instagram:- : synazX.op*            *\n*╚══════🔱🔱🔱🔱🔱═════╝*\n\n*▷Creator: Anees-Muhammed*"
+
+    
+
+    await message.client.sendMessage(
+
+        message.jid,(r_text[1]), MessageType.text);
+
+    }));
+
+        asena.addCommand({pattern: 'git', fromMe: false, deleteCommand: true, desc: GIT,}, (async (message, match) => {
+
+        var r_text = new Array ();
+
+    
+
+        r_text[1] = "*Git links*\n           *\n💥═Sophia Owner Anees-Muhammed═💥*\n\n*💘 https://github.com/Anees-lub/Pikachu*\n*     *\n💓Liyamol═Owner Saidali═*\n\n*⚜https://github.com/saidalisaid2/LiyaMol*    *\n\n⚜yt link to set bot:- https://youtu.be/_D4ZYuUSXjs*"
+
+    
+
+        await message.client.sendMessage(
+
+            message.jid,(r_text[1]), MessageType.text);
+
+    
+
+        }));    
+
+    }
