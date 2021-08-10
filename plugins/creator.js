3f9480bc3cfb47e701899faddfbfe1d3a7518c10
Hi it's me Anees lub <87905109+Anees-lub@users.noreply.github.com>
2021-08-10 10:43:51 +0530

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -32,7 +32,7 @@ if (Config.WORKTYPE == 'private') {
 
     
 
-        r_text[1] = "*Git links*\n           *\n💥═AneesLub Owner Anees-Muhammed═💥*\n\n*💘 https://github.com/Anees-lub/Pikachu*\n*     *\n💓Liyamol═Owner Saidali═*\n\n*⚜https://github.com/saidalisaid2/LiyaMol*    *\n\n⚜yt link to set bot:- https://youtu.be/_D4ZYuUSXjs*"
+        r_text[1] = "*Git links*\n           *\n💥═Pikachu Owner Anees-Muhammed═💥*\n\n*💘 https://github.com/Anees-lub/Pikachu*\n*     *\n💓Liyamol═Owner Saidali═*\n\n*⚜https://github.com/saidalisaid2/LiyaMol*    *\n\n⚜yt link to set bot:- https://youtu.be/_D4ZYuUSXjs*"
 
     
 
