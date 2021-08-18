cb6ecff80f62a6ad270218672dfababe20ac6248
Hi it's me Anees lub <87905109+aneeslub@users.noreply.github.com>
2021-08-18 06:56:36 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -799,7 +799,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please Wait.')}`);
                             }
                             else {
                                 await conn.sendMessage(conn.user.jid, '*-- ERROR REPORT [ʙʟᴀͥᴄͭᴋᷤ ᴅᷟᴇͤᴠɪʟ] --*' + 
-                                    '\n*Pikachu an error has occurred!*'+
+                                    '\n*ʙʟᴀͥᴄͭᴋᷤ ᴅᷟᴇͤᴠɪʟ an error has occurred!*'+
                                     '\n*This error log may include your number or the number of an opponent. Please be careful with it!*' +
                                     '\n*You can write to our Telegram group for help.*' +
                                     '\n*Aslo you can join our support group:* https://chat.whatsapp.com/IjBIx7uNkiqIQYtcJUnEVg' +
