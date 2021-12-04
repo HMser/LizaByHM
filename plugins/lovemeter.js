fef735a10374dc58dea1f6d19b7b30094cbae898
neera-j <86142113+neera-j@users.noreply.github.com>
2021-12-04 16:57:35 +0530

    Update lovemeter.js

--- plugins/lovemeter.js
+++ plugins/lovemeter.js
@@ -1036,7 +1036,7 @@ else {
                 message.jid, 
                 fs.readFileSync("/root/WhatsAsenaDuplicated/media/gif/lmeter.mp4"),
                 MessageType.video, 
-                { mimetype: Mimetype.gif, caption: `*Here is your love percentage towards!* ` + '@' + message.reply_message.jid.split('@')[0] + `🏳️‍🌈\n*Result:* ${r_text[i]}` }
+                { mimetype: Mimetype.gif, caption: `*Do you know how much you Love* ` + '@' + message.reply_message.jid.split('@')[0] + `❤️ \n\n*Well i calculated it and here is how much you love * + '@' + message.reply_message.jid.split('@')[0] + `:` ${r_text[i]} ❤️❤️` }
             )
         }));
     }
