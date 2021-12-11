0920bdf30485ade40543dec3f7996bf2e1e952a4
neera-j <86142113+neera-j@users.noreply.github.com>
2021-12-11 18:46:01 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -7,22 +7,15 @@ const Lang = Language.getString('wallpaper');
 
 Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
 
-    var skl = await axios.get("https://images.wallpapersden.com/image/download/pokemon-detective-pikachu-4k_a2lrZmWUmZqaraWkpJRsZ2WtbGdl.jpg", { responseType: 'arraybuffer' })
+    var skl = await axios.get("https://wallpapercave.com/wp/wp9579723.jpg", { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Pikachu bot created by Ameer Suhail*
-*Creator number : wa.me/919946432377?text=Hi%20Ameer%20Suhail.%20*
-
-*Bot setting video : https://youtu.be/_D4ZYuUSXjs*
-
-*Githublink (Setup)  :    https://github.com/ameer-kallumthodi/pikachu*
-
-*Audio commads :   https://github.com/ameer-kallumthodi/pikachu/tree/master/uploads*
-
-*Sticker commads : https://github.com/ameer-kallumthodi/pikachu/tree/master/uploads*
-
-*Video For Setting Bot : https://youtu.be/_D4ZYuUSXjs* 
-
-*For More Updates Subscribe The Channel ☝*
+    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*≈≈≈≈≈≈≈Links ☟︎︎︎≈≈≈≈≈≈≈≈*
+ 
+*owner number wa.me/918113921898*
+   
+*owner number wa.me/918592061828*
+ 
+ *CONTACT MY OWNERS FOR SOURCE CODE *
 `}) 
 
 }));
