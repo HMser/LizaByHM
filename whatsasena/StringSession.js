977752ed9746df3acd76cc93a884c7f5daf70952
neera-j <86142113+neera-j@users.noreply.github.com>
2021-12-11 22:02:37 +0530

    app.js passed deploy now

--- whatsasena/StringSession.js
+++ whatsasena/StringSession.js
@@ -28,7 +28,7 @@ class StringSession {
     }
 
     createStringSession(dict) {
-        return 'PIKACHU;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
+        return 'LUTTAPPI;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
     }
 }
 
