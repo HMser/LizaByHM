4b5a6a79705e6054df4bb85b4374d448df887a34
neera-j <86142113+neera-j@users.noreply.github.com>
2021-12-12 12:25:29 +0530

    Update delete.js

--- plugins/delete.js
+++ plugins/delete.js
@@ -1,6 +1,6 @@
 const Asena = require('../events');
 const {MessageType} = require('@adiwajshing/baileys');
-const DEL_DESC = "Deletes The Replied Message Send By The Bot [ ✅️ Official External Plugin ]"
+const DEL_DESC = "Deletes The Replied Message Send By The Bot "
 
 Asena.addCommand({pattern: 'del', fromMe: false, desc: DEL_DESC}, (async (message, match) => {
 
