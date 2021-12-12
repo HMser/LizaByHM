4aee93d28aa1c629606b372ab73ca052c64225e5
neera-j <86142113+neera-j@users.noreply.github.com>
2021-12-12 12:59:40 +0530

    Update anime.js

--- plugins/anime.js
+++ plugins/anime.js
@@ -188,7 +188,7 @@ else if (Config.WORKTYPE == 'public') {
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: '*Made By Pikachu*'})
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: '*With Love Frome Luttappi*'})
 
     }));
 }
