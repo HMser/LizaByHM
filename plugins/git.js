f9a3a0a0d0562932b58b74537eea626be2b49c57
Neeraj <95992247+Neeraj-x0@users.noreply.github.com>
2022-01-02 17:25:37 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -7,15 +7,15 @@ const Lang = Language.getString('wallpaper');
 
 Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
 
-    var skl = await axios.get("https://wallpapercave.com/wp/wp9579723.jpg", { responseType: 'arraybuffer' })
+    var skl = await axios.get("https://i.imgur.com/0MwO1yz.jpeg", { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*≈≈≈≈≈≈≈Links ☟︎︎︎≈≈≈≈≈≈≈≈*
- 
+    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*≈≈≈≈≈≈≈≈≈≈☟︎︎︎ Links ☟︎︎︎≈≈≈≈≈≈≈≈≈≈≈≈*
+*≈≈≈≈≈≈≈≈≈≈☟︎︎︎ Links ☟︎︎︎≈≈≈≈≈≈≈≈≈≈≈≈* 
 *owner number wa.me/918113921898*
    
 *owner number wa.me/918592061828*
  
- *CONTACT MY OWNERS FOR SOURCE CODE *
+ *CONTACT MY OWNERS FOR SOURCE CODE*
 `}) 
 
 }));
