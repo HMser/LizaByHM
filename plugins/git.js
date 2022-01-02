604f2fbf55e417c755418f36c6dc2dfa35183e4e
Neeraj <95992247+Neeraj-x0@users.noreply.github.com>
2022-01-02 17:33:15 +0530

    Create git.js

--- plugins/git.js
+++ plugins/git.js
@@ -7,15 +7,8 @@ const Lang = Language.getString('wallpaper');
 
 Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
 
-    var skl = await axios.get("https://i.imgur.com/0MwO1yz.jpeg", { responseType: 'arraybuffer' })
+    var skl = await axios.get("https://i.imgur.com/mJsm8XX.jpeg", { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*≈≈≈≈≈≈≈≈≈≈☟︎︎︎ Links ☟︎︎︎≈≈≈≈≈≈≈≈≈≈≈≈*
-*≈≈≈≈≈≈≈≈≈≈☟︎︎︎ Links ☟︎︎︎≈≈≈≈≈≈≈≈≈≈≈≈* 
-*owner number wa.me/918113921898*
-   
-*owner number wa.me/918592061828*
- 
- *CONTACT MY OWNERS FOR SOURCE CODE*
-`}) 
+    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `https://github.com/Neeraj-x0/Luttappi-WABOT`}) 
 
 }));
