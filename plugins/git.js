7fad94d0a6ea301b03c268fdde8ed996ae943dac
HMSer <95879255+HMser@users.noreply.github.com>
2022-01-17 17:47:55 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -7,8 +7,8 @@ const Lang = Language.getString('wallpaper');
 
 Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
 
-    var skl = await axios.get("https://i.imgur.com/mJsm8XX.jpeg", { responseType: 'arraybuffer' })
+    var skl = await axios.get("https://i.imgur.com/X43MOqu.jpeg", { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `https://github.com/Neeraj-x0/Luttappi-WABOT`}) 
+    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `bit.ly/abouthm`}) 
 
 }));
